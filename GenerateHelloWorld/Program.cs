cdef4bd82f3f4683a8fc82be1d93306fe366221a
WinterThane <demonsaso@gmail.com>
2017-11-14 14:27:29 +0100

    New method
    Added program building method.

--- GenerateHelloWorld/Program.cs
+++ GenerateHelloWorld/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.CodeDom;
 
 namespace GenerateHelloWorld
 {
     class Program
     {
+        static CodeNamespace BuildProgram()
+        {
+            var nameSpc = new CodeNamespace("GeneratedWorld");
+            var systemImport = new CodeNamespaceImport("System");
+            nameSpc.Imports.Add(systemImport);
+            var programClass = new CodeTypeDeclaration("Program");
+            nameSpc.Types.Add(programClass);
+            var methodMain = new CodeMemberMethod
+            {
+                Attributes = MemberAttributes.Static,
+                Name = "Main"
+            };
+            methodMain.Statements.Add(new CodeMethodInvokeExpression(new CodeSnippetExpression("Console"), "WriteLine", new CodePrimitiveExpression("Hello generated world!")));
+            programClass.Members.Add(methodMain);
+
+            return nameSpc;
+        }
+
         static void Main(string[] args)
         {
+
+
+            Console.ReadLine();
         }
     }
 }
